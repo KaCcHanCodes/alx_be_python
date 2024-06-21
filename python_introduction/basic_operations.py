f0fe96d670d738e523e907c744ff08960445b892
Ugochukwu <emdeefx@gmail.com>
2024-06-21 14:59:31 +0100

    Added a new file basic_operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+value1 = 0
+value2 = 0
+value3 = 0
+
+value1 = number1 + number2
+print("Addition of", number1, "and", number2, "is", value1)
+
+value2 = number1 - number2
+print("Subtraction of", number1, "and", number2, "is", value2)
+
+value3 = number1 * number2
+print("Multiplication of", number1, "and", number2, "is", value3)
