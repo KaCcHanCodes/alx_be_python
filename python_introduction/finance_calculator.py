7325bd6f63413b43d1c2be4663c86db95db49c6b
Ugochukwu <emdeefx@gmail.com>
2024-06-21 19:02:17 +0100

    made corrections to variable naming and removing type casting in file financial_calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,8 @@
-m_income = int(input("Enter your monthly income: "))
-m_expenses = int(input("Enter your total monthly expenses: "))
-m_savings = 0
-a_savings = 0
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
 
-m_savings = m_income - m_expenses
-print(f"Your monthly savings are ${m_savings}.")
-a_savings = m_savings * 12 + (m_savings * 12 * 0.05)
-print(f"Projected savings after one year, with interest, is: ${int(a_savings)}.")
+monthly_savings = monthly_income - monthly_expenses
+print(f"Your monthly savings are ${monthly_savings}.")
+
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
