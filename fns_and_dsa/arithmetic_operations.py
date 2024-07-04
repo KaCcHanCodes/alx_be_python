2f1de4567f1e885095bb60379aa5bf011bc0a90b
Ugochukwu <emdeefx@gmail.com>
2024-07-04 19:20:07 +0100

    Create new dir fns_and_dsa. practice constructing functions and data structures

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,9 @@
+def perform_operation(num1, num2, operation):
+    match operation:
+        case "add": return num1 + num2
+        case "subtract": return num1 - num2
+        case "multiply": return num1 * num2
+        case "divide": 
+            if (num1 == 0) or (num2 == 0):
+                return "Invalid division"
+            return num1 / num2
