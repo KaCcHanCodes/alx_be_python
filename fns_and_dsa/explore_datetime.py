55411b8d590ca9f5a08582cbaf6c3dd11db101bf
Ugochukwu <emdeefx@gmail.com>
2024-07-04 23:39:26 +0100

    Create functions to print the current date and time, and the future date when given a value

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta, date
+
+def display_current_datetime():
+    date = datetime.now() 
+    current_date = date.strftime("%Y-%m-%d %H:%M:%S")
+    
+    print(f"Current date and time: {current_date}")
+
+
+def calculate_future_date():
+    number_of_days = int(input("Enter the number of days to add to the current date: "))
+    delta = timedelta(days=number_of_days)
+    day = date.today()
+    future_date = day + delta
+
+    print(f"Future date: {future_date}")
+
+display_current_datetime()
+calculate_future_date()
