8448d020646f8ac39a625a20d46f233122e7520e
Ugochukwu <emdeefx@gmail.com>
2024-07-05 00:07:59 +0100

    Trying to test if the checker will pass my code

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -9,9 +9,10 @@ def display_current_datetime():
 
 def calculate_future_date():
     number_of_days = int(input("Enter the number of days to add to the current date: "))
-    delta = timedelta(days=number_of_days)
-    day = date.today()
+    delta = datetime.timedelta(days=number_of_days)
+    day = datetime.today()
     future_date = day + delta
+    result = future_date.strftime()
 
     print(f"Future date: {future_date}")
 
