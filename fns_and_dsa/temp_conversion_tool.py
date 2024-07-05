9fd7a030040e93a91d3d6e103d6557271973ae15
Ugochukwu <emdeefx@gmail.com>
2024-07-05 12:23:23 +0100

    Add the .isdigit method to check if the value provided is a numerical value

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,7 +1,7 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
-temp = int(input("Enter the temperature to convert: "))
+temp = (input("Enter the temperature to convert: "))
 unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
 
 def convert_to_celsius(fahrenheit):
@@ -12,10 +12,14 @@ def convert_to_fahrenheit(celsius):
     fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     return fahrenheit
 
-if unit == "F":
-    conversion = convert_to_celsius(temp)
-    print(f"{float(temp)}°F is {conversion}°C")
-elif unit == "C":
-    conversion = convert_to_fahrenheit(temp)
-    print(f"{float(temp)}°C is {conversion}°F")
-else: print("Invalid unit")
+if temp.isdigit():
+    temp = int(temp)
+    if unit == "F":
+        conversion = convert_to_celsius(temp)
+        print(f"{float(temp)}°F is {conversion}°C")
+    elif unit == "C":
+        conversion = convert_to_fahrenheit(temp)
+        print(f"{float(temp)}°C is {conversion}°F")
+    else: print("Invalid unit")
+else:
+    print("Invalid temperature. Please enter a numeric value.")
