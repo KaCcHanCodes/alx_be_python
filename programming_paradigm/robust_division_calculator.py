01fb48d2eefa541043234d64d9c1e3404eb613d2
Ugochukwu <emdeefx@gmail.com>
2024-07-11 20:38:22 +0100

    Fix error raise ZeroDivisionError. Uncomment from line 9 - 14

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,13 +2,13 @@ def safe_divide(numerator, denominator):
     try:
         num = float(numerator)
         den = float(denominator)
-        if num != 0:
-            return f"The result of the division is {num / den}"
-        elif num == 0 or den == 0:
+        if num == 0 or den == 0:
             raise ZeroDivisionError("Error: Cannot divide by zero.")
+        else:
+            return f"The result of the division is {num / den}"
     except ValueError:
         return "Error: Please enter numeric values only."
-    # except ZeroDivisionError as e:
-    #     return print(e)
-    # except Exception as e:
-    #    return print(e)
+    except ZeroDivisionError as e:
+        return e
+    except Exception as e:
+       return e
