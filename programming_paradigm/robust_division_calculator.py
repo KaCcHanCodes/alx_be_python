e9ca72345e6f06def56d689e0f351c02bad9aa71
Ugochukwu <emdeefx@gmail.com>
2024-07-11 18:47:32 +0100

    Store the value of the division in the variable result in line 7.

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,6 +4,7 @@ def safe_divide(numerator, denominator):
         if numerator == 0 or denominator == 0:
             raise ZeroDivisionError("Error: Cannot divide by zero.")
         else:
-            print(f"The result of the division is {numerator / denominator}")
+            result = numerator / denominator
+            print(f"The result of the division is {result}")
     except ValueError:
         print("Error: Please enter numeric values only.")
