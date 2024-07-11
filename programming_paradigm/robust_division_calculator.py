1a55a527989fbe97b7694ddc896eb3b7ed8e9aa9
Ugochukwu <emdeefx@gmail.com>
2024-07-11 18:38:05 +0100

    Typecast input variables to float in line 2.

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,9 +1,10 @@
 def safe_divide(numerator, denominator):
+    float(numerator), float(denominator)
     try:
         if numerator == 0 or denominator == 0:
             raise ZeroDivisionError("Error: Cannot divide by zero.")
         else:
-            result = print(f"The result of the division is {float(numerator / denominator)}")
+            result = print(f"The result of the division is {numerator / denominator}")
             return result
     except ValueError:
         print("Error: Please enter numeric values only.")
