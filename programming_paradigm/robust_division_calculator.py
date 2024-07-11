43678dd09db94e6aed6b9a4681d35b61b644cd47
Ugochukwu <emdeefx@gmail.com>
2024-07-11 18:29:06 +0100

    Practice writing error handling code

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,9 @@
+def safe_divide(numerator, denominator):
+    try:
+        if numerator == 0 or denominator == 0:
+            raise ZeroDivisionError("Error: Cannot divide by zero.")
+        else:
+            result = print(f"The result of the division is {numerator / denominator}")
+            return result
+    except ValueError:
+        print("Error: Please enter numeric values only.")
