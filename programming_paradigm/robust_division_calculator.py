ce51aac391fd12df402ba86011d09ffbf54c694e
Ugochukwu <emdeefx@gmail.com>
2024-07-11 18:44:39 +0100

    Remove the return function, print the result instead

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,7 +4,6 @@ def safe_divide(numerator, denominator):
         if numerator == 0 or denominator == 0:
             raise ZeroDivisionError("Error: Cannot divide by zero.")
         else:
-            result = print(f"The result of the division is {numerator / denominator}")
-            return result
+            print(f"The result of the division is {numerator / denominator}")
     except ValueError:
         print("Error: Please enter numeric values only.")
