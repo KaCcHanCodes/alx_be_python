cd27b9325deeb32856188437954cdc5b0249809e
Ugochukwu <emdeefx@gmail.com>
2024-07-11 18:34:59 +0100

    Typecast division result in line 6.

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,7 +3,7 @@ def safe_divide(numerator, denominator):
         if numerator == 0 or denominator == 0:
             raise ZeroDivisionError("Error: Cannot divide by zero.")
         else:
-            result = print(f"The result of the division is {numerator / denominator}")
+            result = print(f"The result of the division is {float(numerator / denominator)}")
             return result
     except ValueError:
         print("Error: Please enter numeric values only.")
