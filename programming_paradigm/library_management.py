465af185c76e84c220d37de855a83ea459433084
Ugochukwu <emdeefx@gmail.com>
2024-07-14 10:01:16 +0100

    Restructure code to make it more understandable, and to satisfy the checker

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -2,17 +2,17 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
-        self._is_checked_out = False
+        self._is_checked_out = False #default attribute
 
-    def check_out_book(self, book_title):
-        #checks if the book title is same as book_title, if yes set _is_checked_out as true
+    def check_out(self, book_title):
+        #checks if the book title is same as book_title, if yes the state of _is_checked_out becomes true
         if self.title == book_title:
             self._is_checked_out = True
             return True
         return False
 
     def return_book(self, book_title):
-        #checks if the book title is same as book_title, if yes set _is_checked_out as false
+        #checks if the book title is same as book_title, if yes the state of _is_checked_out becomes false
         if self.title == book_title:
             self._is_checked_out = False
             return True
@@ -20,25 +20,29 @@ class Book:
 
 class Library:
     def __init__(self):
-        self._books = []
+        self._books = [] #Book shelf
 
     def add_book(self, book):
         self._books.append(book)
     
     def list_available_books(self):
     #Create a new list to filter books that are still available(_is_checked_out = False)
-        available_books = [book for book in self._books if not book._is_checked_out]
-        for book in available_books: #Iterate through the new list of books in the library
-            print(f"{book.title} by {book.author}")
+        for book in self._books:
+            if book._is_checked_out == False:
+                available_books = [book]
+                for book in available_books: #Iterate through the new list of books in the library
+                    print(f"{book.title} by {book.author}")
 
     def check_out_book(self, book_title):
         #Iterate through the shelf (self._books)
         for book in self._books:
-        #Runs every book in the shelf and cross reference the book_title value with the check_out_book method in the class Book    
-            if book.check_out_book(book_title):  
+        #Runs every book in the shelf and cross reference the book_title with book titles in the #Book shelf and checks out using the check_out method in the class Book    
+            if book.title == book_title and not book._is_checked_out:
+                book.check_out(book_title)  
                 return
     def return_book(self, book_title):
-    #Runs every book in the shelf and cross reference the book_title value with the return_book method in the class Book 
+    #Runs every book in the shelf and cross reference the book_title value with book titles in the #Book shelf and returns them using the return_book method in the class Book 
         for book in self._books:
-            if book.return_book(book_title):
+            if book.title == book_title and book._is_checked_out:
+                book.return_book(book_title)
                 return
