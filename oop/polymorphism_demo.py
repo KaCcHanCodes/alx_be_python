839734f7e85155eb9f3648b69252767761ec52eb
Ugochukwu <emdeefx@gmail.com>
2024-07-18 22:40:44 +0100

    Create polymorphism_demo.py. Practice method overriding and class polymorphic behaviour

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,21 @@
+import math
+class Shape:
+    def area(self):
+        raise NotImplementedError
+    
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        result = self.length * self.width
+        return result
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        result = math.pi * self.radius**2
+        return result
